4a7a02979a021f75caf08350bce17857b1522ea2
Adrian Karnani <adriankarnani@gmail.com>
2022-08-27 17:40:12 -0400

    added controller logic for follow/unfollow

--- APIs/usersAPI/controllers/userController.js
+++ APIs/usersAPI/controllers/userController.js
@@ -5,7 +5,6 @@ const SALT_WORK_FACTOR = 10;
 
 const userController = {};
 
-
 userController.checkUser = async (req, res, next) => {
   // this is only used when a user signs up for a new account
   // gets email from request body
@@ -23,17 +22,20 @@ userController.checkUser = async (req, res, next) => {
     if (checkMail.rows.length > 0) {
       // return res.status(403).send('There is already an account associated with this email address.');
       res.locals.mailError = true;
-      console.error('There is already an account associated with this email address.');
-    };
+      console.error(
+        'There is already an account associated with this email address.'
+      );
+    }
 
     return next();
   } catch (err) {
     return next({
       log: `userController.checkUser: ERROR ${err}`,
-      message: { err: 'userController.checkUser: ERROR: Check server logs for details.' }
+      message: {
+        err: 'userController.checkUser: ERROR: Check server logs for details.'
+      }
     });
   }
-
 };
 userController.createUser = async (req, res, next) => {
   // takes in username, password, email, firstname, lastname, city from request
@@ -43,7 +45,7 @@ userController.createUser = async (req, res, next) => {
   try {
     const { email, password, firstName, lastName, city } = req.body;
     // if (!regex.test(username) || !regex.test(password)) throw 'Username and/or password are blank.';
-    console.log(req.body)
+    console.log(req.body);
     const hashPassword = await bcrypt.hash(password, SALT_WORK_FACTOR);
     console.log(hashPassword);
     const sqlQuery = `
@@ -52,13 +54,21 @@ userController.createUser = async (req, res, next) => {
     RETURNING
     _id, first_name, last_name, email, city`;
 
-    const response = await db.query(sqlQuery, [firstName, lastName, email, city, hashPassword]);
+    const response = await db.query(sqlQuery, [
+      firstName,
+      lastName,
+      email,
+      city,
+      hashPassword
+    ]);
     res.locals = response.rows[0];
     return next();
   } catch (error) {
     return next({
       log: `userController.createUser: ERROR: ${error}`,
-      message: { err: 'userController.createUser: ERROR: Check server logs for details.' }
+      message: {
+        err: 'userController.createUser: ERROR: Check server logs for details.'
+      }
     });
   }
 };
@@ -78,14 +88,22 @@ userController.updateUser = async (req, res, next) => {
     imageUrl = $6
     RETURNING *`;
 
-    const response = await db.query(updateUserQuery, [firstName, lastName, email,
-      city, info, imageUrl]);
+    const response = await db.query(updateUserQuery, [
+      firstName,
+      lastName,
+      email,
+      city,
+      info,
+      imageUrl
+    ]);
     res.locals = response.rows[0];
     return next();
   } catch (error) {
     return next({
       log: `userController.updateUser: ERROR: ${error}`,
-      message: { err: 'userController.updateUser: ERROR: Check server logs for details.' }
+      message: {
+        err: 'userController.updateUser: ERROR: Check server logs for details.'
+      }
     });
   }
 };
@@ -104,7 +122,10 @@ userController.loginUser = async (req, res, next) => {
 
     const response = await db.query(loginQuery, [email]);
     const match = await bcrypt.compare(password, response.rows[0].password);
-    if (!match) return res.status(403).send('You have entered invalid email or password.');
+    if (!match)
+      return res
+        .status(403)
+        .send('You have entered invalid email or password.');
     res.locals = {
       user_id: response.rows[0]._id,
       email: response.rows[0].email,
@@ -114,14 +135,15 @@ userController.loginUser = async (req, res, next) => {
       info: response.rows[0].info,
       profile_image_url: response.rows[0].profile_image_url,
       num_supporters: response.rows[0].num_supporters,
-      created_at: response.rows[0].created_at,
+      created_at: response.rows[0].created_at
     };
     return next();
-  }
-  catch (error) {
+  } catch (error) {
     return next({
       log: `userController.loginUser: ERROR: ${error}`,
-      message: { err: 'userController.loginUser: ERROR: Check server logs for details.' }
+      message: {
+        err: 'userController.loginUser: ERROR: Check server logs for details.'
+      }
     });
   }
 };
@@ -130,7 +152,6 @@ userController.findOneByUserId = async (req, res, next) => {
   // req.user is set up in the jwt.verifyToken controller
   const userId = req.user;
   try {
-
     const findUserIdQuery = `
     SELECT
 	    *
@@ -144,30 +165,82 @@ userController.findOneByUserId = async (req, res, next) => {
       throw {
         log: `User with id: ${userId} could not be found.`,
         status: 404,
-        message: `User with id: ${userId} could not be found.`,
+        message: `User with id: ${userId} could not be found.`
       };
-    };
+    }
 
     res.locals = {
       user_id: response.rows[0]._id,
       first_name: response.rows[0].first_name,
-      last_name: response.rows[0].last_name,
+      last_name: response.rows[0].last_name
     };
     return next();
   } catch (error) {
     return next({
       log: `userController.findOneByUserId: ERROR: ${error.log}`,
-      message: { err: `${error.message ? error.message : 'userController.findOneByUserId: ERROR: Check server logs for details.'}` },
-      status: error.status ? error.status : 500,
+      message: {
+        err: `${
+          error.message
+            ? error.message
+            : 'userController.findOneByUserId: ERROR: Check server logs for details.'
+        }`
+      },
+      status: error.status ? error.status : 500
     });
   }
 };
 
 // follow user by id
-userController.followUser = async (req, res, next) => {};
+userController.followUser = async (req, res, next) => {
+  const { followeeId } = req.body;
+  const followerId = req.user;
+
+  try {
+    const followUserQuery = `
+    INSERT INTO followee_follower (followee_id, follower_id)
+    VALUES ($1, $2)
+    RETURNING *`;
+
+    const response = await db.query(followUserQuery, [followeeId, followerId]);
+    res.locals.follow = response.rows;
+
+    return next();
+  } catch (error) {
+    return next({
+      log: `userController.followUser: ERROR: ${error}`,
+      message: {
+        err: 'userController.followUser: ERROR: Check server logs for details.'
+      }
+    });
+  }
+};
 
 // unfollow user by id
-userController.unfollowUser = async (req, res, next) => {};
+userController.unfollowUser = async (req, res, next) => {
+  const { followeeId } = req.body;
+  const followerId = req.user;
+
+  try {
+    const unfollowUserQuery = `
+    DELETE FROM followee_follower
+    WHERE followee_id = $1 AND follower_id = $2`;
+
+    const response = await db.query(unfollowUserQuery, [
+      followeeId,
+      followerId
+    ]);
+    res.locals.follow = response.rows;
+
+    return next();
+  } catch (error) {
+    return next({
+      log: `userController.unfollowUser: ERROR: ${error}`,
+      message: {
+        err: 'userController.unfollowUser: ERROR: Check server logs for details.'
+      }
+    });
+  }
+};
 
 // retrieve all users followed by a particular user
 userController.getFollowees = async (req, res, next) => {};
