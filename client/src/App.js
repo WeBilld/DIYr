0477535708afc09f6e5048018375d205219e9178
MichaelCSnyder <msnyder1992@gmail.com>
2022-08-27 14:06:56 -0400

    Setup initial react router structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home'
 import Profile from './pages/profile/Profile'
 
 export default function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-      <Profile />
-      {/* <Home /> */}
-    </>
+      <Routes>
+          <Route exact path='/' element={<Home />}></Route>
+          <Route path='/profile' element={<Profile />}></Route>
+          <Route path='/profile/:id' element={<Profile />}></Route>
+          {/* <Profile /> */}
+          {/* <Home /> */}
+      </Routes>
+    </BrowserRouter>
+
   )
 }
