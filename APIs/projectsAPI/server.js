ec999d7d05aac24047987af721d71c378bd3e327
Gerry Bong <ggbong734@gmail.com>
2022-08-27 10:58:40 -0700

    first draft of projects API Get requests

--- APIs/projectsAPI/server.js
+++ APIs/projectsAPI/server.js
@@ -34,6 +34,11 @@ const ProjectType = new GraphQLObjectType({
     image_url: { type: GraphQLString },
     num_likes: { type: GraphQLInt },
     created_at: { type: GraphQLString },
+    city: { type: GraphQLString },
+    first_name: { type: GraphQLString },
+    last_name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    liked_by_user: { type: GraphQLBoolean },
   }),
 });
 
@@ -45,27 +50,84 @@ const RootQueryType = new GraphQLObjectType({
   fields: () => ({
     getUserProjects: {
       type: new GraphQLList(ProjectType),
+      description: "get all projects for a user",
+      args: {
+        user_id: { type: GraphQLNonNull(GraphQLInt) }
+      }, //query parameter gets stored here
+      resolve: async (parent, { user_id }) => {
+        try {
+          let getUserProjectsQuery = `
+          SELECT p.*, u.city, u.first_name, u.last_name, u.email,
+          EXISTS (SELECT * FROM likes l WHERE l.project_id = p._id AND l.user_id = $1) AS liked_by_user
+          FROM PROJECTS AS p JOIN USERS AS u
+          ON p.owner_id = u._id
+          WHERE u._id = $1
+          ORDER BY p.created_at DESC`;
+          let response = await db.query(getUserProjectsQuery, [user_id]);
+          console.log(` response of the getUserProjectsQuery is`, response.rows);
+          return response.rows[0];
+        }
+        catch (error) {
+          return `projectsController.getUserProjects: ERROR: ${error}`;
+        }
+      }
     },
     getLocalProjects: {
-
+      type: new GraphQLList(ProjectType),
+      description: "get all projects from a city",
+      args: {
+        user_id: { type: GraphQLNonNull(GraphQLInt) },
+        city: { type: GraphQLNonNull(GraphQLString) }
+      },
+      resolve: async (parent, { city }) => {
+        try {
+          let getLocalProjectsQuery = `
+          SELECT p.*, u.city, u.first_name, u.last_name, u.email,
+          EXISTS (SELECT * FROM likes l WHERE l.project_id = p._id AND l.user_id = $1) AS liked_by_user
+          FROM projects p INNER JOIN users u ON p.owner_id = u._id
+          WHERE u.city = $2 ORDER BY p.created_at;`;
+          let response = await db.query(getLocalProjectsQuery, [user_id, city]);
+          console.log(`response of the getLocalProjectsQuery is`, response.rows);
+          return response.rows[0];
+        }
+        catch (error) {
+          return `projectsController.getLocalProjects: ERROR: ${error}`;
+        }
+      }
     },
     getFolloweesProjects: {
-
+      type: new GraphQLList(ProjectType),
+      description: "get all projects from a person the user follows",
+      args: {
+        user_id: { type: GraphQLNonNull(GraphQLInt) },
+      },
+      resolve: async (parent, { city }) => {
+        try {
+          let getFolloweesProjectsQuery = `
+          SELECT p.*, u.city, u.first_name, u.last_name, u.email,
+          EXISTS (SELECT * FROM likes l WHERE l.project_id = p._id AND l.user_id = $1) AS liked_by_user
+          FROM PROJECTS p
+          INNER JOIN users u ON p.owner_id = u._id
+          WHERE p.owner_id IN (
+            SELECT f.followee_id FROM FOLLOWEE_FOLLOWER AS f
+            WHERE f.follower_id = $1)
+          ORDER BY p.created_at DESC`;
+          let response = await db.query(getFolloweesProjectsQuery, [user_id]);
+          console.log(`response of the getFolloweesProjectsQuery is`, response.rows);
+          return response.rows[0];
+        }
+        catch (error) {
+          return `projectsController.getFolloweesProjects: ERROR: ${error}`;
+        }
+      }
     }
 
   })
 })
 
-
-
-
-
-
-
-
-
 const schema = new GraphQLSchema({
   query: RootQueryType,
+  // mutation:
 });
 
 app.use(
