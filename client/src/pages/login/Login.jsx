2d1695885a590a1a09e8c41d56f7dfad969f8340
Jonathan Luu <jonathanluu17@gmail.com>
2022-08-27 10:52:51 -0700

    login first iteration complete, missing onclick methods for buttons

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,8 +1,10 @@
 import './login.css';
+import React, {Component, useEffect} from 'react'
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 export default function Login() {
     const [inputs, setInputs] = React.useState({
@@ -10,28 +12,47 @@ export default function Login() {
         password: ''
     });
 
+    // handle change of inputs
+    const handleInputChange = (prop) => (e) => {
+        setInputs({...inputs, [prop]: e.target.value});
+    }
+
+    // onClick for login to submit to our auth api
+    
 
     return (
         <div className="loginMain">
-            <Container className = "loginMainContainer" maxWidth = "sm" sx = {{p:2}}>
                 <Box
                     className = "loginContainer"
-                    sx = {{ p: 2 }}
+                    sx = {{ p: 2, mr:100 }}
+                    alignItems="left"
                     >
-                        <Stack className="loginFields" direction="column" spacing={3} justifyContent="center" alignItems="center">
-                            <TextField
-                                label="Email"
-                                variant="standard"
-                                fullWidth
-                            />
-                            <TextField
-                                label="Password"
-                                variant="standard"
-                                fullWidth
-                            />
-                        </Stack>
+                        <Stack className="mainStack" direction="column" spacing={3} justifyContent="center" alignItems="center">
+                            <span className ="bigLogo">DIYr</span>
+                            <h2 className="app description">Do it yourself, with others</h2>
+                            <Box className = "inputsContainer" sx={{p:3, boxShadow:1, minWidth:350}}>
+                                <Stack className="loginFields" direction="column" spacing={3} justifyContent="center" alignItems="center">
+                                    <TextField
+                                        label="Email"
+                                        variant="standard"
+                                        fullWidth
+                                        onChange={handleInputChange('email')}
+                                    />
+                                    <TextField
+                                        label="Password"
+                                        variant="standard"
+                                        type="password"
+                                        fullWidth
+                                        onChange={handleInputChange('password')}
+                                    />
+                                    <Stack className="submitButtonsStack" direction="row" spacing={3} justifyContent="center">
+                                        <Button variant="contained" size="large">Log In</Button>
+                                        <Button variant="contained" size="large">Sign Up</Button>
+                                    </Stack>
+                                </Stack>
+                            </Box>
+                            </Stack>
                 </Box>
-            </Container>
         </div>
     )
 }
