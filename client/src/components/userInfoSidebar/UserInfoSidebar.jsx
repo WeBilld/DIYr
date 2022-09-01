628c162e2748da6c92c07d38db5a8bd67c97b1bc
MichaelCSnyder <msnyder1992@gmail.com>
2022-09-01 14:31:43 -0400

    Wire userInfoSidebar to user info in state

--- client/src/components/userInfoSidebar/UserInfoSidebar.jsx
+++ client/src/components/userInfoSidebar/UserInfoSidebar.jsx
@@ -1,23 +1,27 @@
 import { Button } from '@mui/material';
+import { useContext } from 'react';
+import UserContext from '../../Contexts/UserContext';
 import './userInfoSidebar.css';
 
 export default function UserInfoSidebar() {
+    //const { userInfo, setUserInfo } = useContext(UserContext)
+    const { userInfo } = useContext(UserContext);
     return (
         <div className="userInfoSidebar">
             <div className="sidebarWrapper">
-                <img src="https://images.unsplash.com/photo-1524117074681-31bd4de22ad3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt="" className="profileImg" />
+                <img src={userInfo.profile_image_url} alt="" className="profileImg" />
                 <div className="sidebarInfo">
                     <div className="infoItem">
                         <p className="infoCategory">Name: </p>
-                        <p className='infoAnswer'>Lesley Pierce</p>
+                        <p className='infoAnswer'>{userInfo.first_name + ' ' + userInfo.last_name}</p>
                     </div>
                     <div className="infoItem">
                         <p className="infoCategory">City: </p>
-                        <p className='infoAnswer'>Chicago</p>
+                        <p className='infoAnswer'>{userInfo.city}</p>
                     </div>
                     <div className="infoItem">
                         <p className="infoCategory">About Me: </p>
-                        <p className='infoAnswer'>I love building and restoring furniture! Feel free to follow me and see what I'm up to.</p>
+                        <p className='infoAnswer'>{userInfo.info}</p>
                     </div>
                     <div className="buttonWrapper">
                         <Button variant="text" id="signoutButton">Signout</Button>
